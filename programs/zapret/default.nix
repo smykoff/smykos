65308b04cf8ded8a18dbe92d60643b2bdef34d22
smykoil <smykoil@ya.ru>
2026-03-02 16:16:52 +0300

    move zapret conf

---
+++ programs/zapret/default.nix
@@ -0,0 +1,76 @@
+{ unstable, ... }:
+
+{
+  environment.systemPackages = with unstable; [ zapret ];
+
+  services.zapret = {
+    enable = true;
+
+    package = unstable.zapret;
+
+    params = [
+      # "--filter-tcp=443,2053,2083,2087,2096,8443,31337-31338"
+      # "--filter-udp=443,19294-19344,50000-50100,31337-31338"
+      # "--filter-udp=443"
+      # "--hostlist=/etc/zapret/list-general.txt"
+      # "--hostlist-exclude=/etc/zapret/list-exclude.txt"
+      # "--ipset-exclude=/etc/zapret/ipset-exclude.txt"
+
+      # "--dpi-desync=fake --dpi-desync-fooling=datanoack --dpi-desync-fake-tls=0x1603 --dpi-desync-fake-tls=!+2 --dpi-desync-fake-tls-mod=rnd,dupsid,rndsni --dpi-desync-fake-tcp-mod=seq"
+
+      # "--dpi-desync=fake --dpi-desync-fooling=ts --dpi-desync-fake-tls=0x00000000 --dpi-desync-fake-tls=! --dpi-desync-fake-tls-mod=rnd,rndsni,dupsid"
+      # "--dpi-desync=fake --dpi-desync-fooling=md5sig --dup=1 --dup-cutoff=n2 --dup-fooling=md5sig"
+
+      "--dpi-desync-fake-tls=0x1603 --dpi-desync-fake-tls=!+2 --dpi-desync-fake-tls-mod=rnd,dupsid,rndsni"
+      # "--dpi-desync-fake-tcp-mod=seq"
+
+      # "--filter-tcp=443,2053,2083,2087,2096,8443,31337-31338"
+      # "--filter-udp=443,19294-19344,50000-50100,31337-31338"
+      # "--filter-udp=443"
+      # "--hostlist=/etc/zapret/list-general.txt"
+      # "--hostlist-exclude=/etc/zapret/list-exclude.txt"
+      # "--ipset-exclude=/etc/zapret/ipset-exclude.txt"
+      # "--dpi-desync=fake"
+      # "--dpi-desync-repeats=6"
+      # "--dpi-desync-fake-quic=/etc/zapret/quic_initial_www_google_com.bin"
+      #
+      # "--filter-udp=19294-19344,50000-50100"
+      # "--filter-l7=discord,stun"
+      # "--dpi-desync=fake"
+      # "--dpi-desync-repeats=6"
+      #
+      # "--filter-l3=ipv4"
+      # "--filter-tcp=443,2053,2083,2087,2096,8443,31337-31338"
+      # "--hostlist-exclude=/etc/zapret/list-exclude.txt"
+      # "--ipset-exclude=/etc/zapret/ipset-exclude.txt"
+      # "--dpi-desync=syndata,multidisorder"
+      #
+      # "--filter-udp=443"
+      # "--ipset=/etc/zapret/ipset-all.txt"
+      # "--hostlist-exclude=/etc/zapret/list-exclude.txt"
+      # "--ipset-exclude=/etc/zapret/ipset-exclude.txt"
+      # "--dpi-desync=fake"
+      # "--dpi-desync-repeats=6"
+      # "--dpi-desync-fake-quic=/etc/zapret/quic_initial_www_google_com.bin"
+      #
+      # "--filter-udp=31337-31338"
+      # "--ipset=/etc/zapret/ipset-all.txt"
+      # "--ipset-exclude=/etc/zapret/ipset-exclude.txt"
+      # "--dpi-desync=fake"
+      # "--dpi-desync-autottl=2"
+      # "--dpi-desync-repeats=14"
+      # "--dpi-desync-any-protocol"
+      # "--dpi-desync-fake-unknown-udp=/etc/zapret/quic_initial_www_google_com.bin"
+      # "--dpi-desync-cutoff=n3"
+    ];
+  };
+
+  environment.etc = {
+    "zapret/list-general.txt".source = ./list-general.txt;
+    "zapret/list-exclude.txt".source = ./list-exclude.txt;
+    "zapret/ipset-exclude.txt".source = ./ipset-exclude.txt;
+    "zapret/ipset-all.txt".source = ./ipset-all.txt;
+    "zapret/quic_initial_www_google_com.bin".source =
+          ./quic_initial_www_google_com.bin;
+  };
+}
